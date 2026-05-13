01c89458a27ea5c59b5fd6f25523626bcf538efc
marsindex-protocol <marsindex.io@proton.me>
2026-05-14 02:56:06 +0800

    Upload clean V2 landing page

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'MARSINDEX',
+  description: 'Genesis Index Protocol',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="bg-black text-white font-sans">{children}</body>
+    </html>
+  )
+}
